e4b967fdf078dc76bf8c3b3895a962822894a069
José <chocomouuu@gmail.com>
2021-11-18 19:45:31 -0600

    Feat - Cambio nombre de repo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const numero = 35;
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {numero === 1 ? "chido" : 'Nel'}
         </a>
       </header>
     </div>
